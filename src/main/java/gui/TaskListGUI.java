084845774ce1ed31ac73def3a51bd3b7c96f2d1e
Kenz <kenzmarie.exe@gmail.com>
2026-03-30 10:09:36 -0400

    Cambio de archivos de GUI a JFrame

--- src/main/java/gui/TaskListGUI.java
+++ src/main/java/gui/TaskListGUI.java
@@ -1,7 +1,42 @@
 package gui;
 
-/*GUI DE LA LISTA DE TAREAS*/
+import java.awt.EventQueue;
 
-public class TaskListGUI {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class TaskListGUI extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TaskListGUI frame = new TaskListGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TaskListGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+	}
 
 }
