42274f4f631a305ad0e4a5457f5c77dfd928305c
dls_Samuel <diolyssamuel651@gmail.com>
2026-04-03 08:45:21 -0400

    diolys commit to tasklist

--- src/main/java/tasklist/RepeatingTask.java
+++ src/main/java/tasklist/RepeatingTask.java
@@ -2,12 +2,15 @@ package tasklist;
 
 /*FUNCIONALIDAD DE LAS TAREAS QUE SE REPITEN*/
 
-public class RepeatingTask extends TaskListAbstract {
+public abstract class RepeatingTask extends TaskListAbstract {
 	
 	//Constructor
 	public RepeatingTask(String taskName, String taskDescription) {
 		super(taskName, taskDescription);
 		// TODO Auto-generated constructor stub
 	}
-
+    @Override 
+	public String toString() {
+		return "[REpetitiva]" + taskName + "-" + taskDescription + "(" + taskDescription + ")";
+	}
 }
