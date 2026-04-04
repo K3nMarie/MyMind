d05b988e3066eaa328a869b1e7c890d157d5679f
dls_Samuel <diolyssamuel651@gmail.com>
2026-04-04 14:15:08 -0400

    TaskList/condition: awaiting review and confirmation. y Journal/ condition: awaiting review and confirmation.

--- src/main/java/journal/JournalFunc.java
+++ src/main/java/journal/JournalFunc.java
@@ -5,10 +5,31 @@ package journal;
 public class JournalFunc {
 	
 	//Atributos
+	int id; //puse id para que la base de datos pueda indentificar las tareas del diario 
 	String entryTitle; //El titulo de la entrada de diario
 	String entryContent; //El contenido de la entrada de diario
 	//ENTRY DATE
-	
+	public JournalFunc() {
+		
+	}
+	public JournalFunc ( String Title, String content) {
+		
+		this.entryTitle= Title;
+		this.entryContent = content;
+	}
 	//Metodos
+	public int getid() { return id;}
+	public void setentryid(int id) {
+		this.id = id;
+	}
+	
+	public String getentryTitle() {	return entryTitle;}
+	public void setentryTitle(String entryTitle) {
+		this.entryTitle= entryTitle;
+	}
+	public String getentryContent(){ return entryContent; }
+	public void setentryContent(String entryContent) {
+		this.entryTitle= entryContent;
+	}
 
 }
