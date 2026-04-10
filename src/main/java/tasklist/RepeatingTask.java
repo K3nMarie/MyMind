0f7cce182fe80967da936c6a3b6ad40a4d0b5817
dls_Samuel <diolyssamuel651@gmail.com>
2026-04-10 16:44:39 -0400

    the holy  TaskList the reason  por la que dure mucho tiempo, sorry ya lo restante es infinitas veces mas rapido de hacer para mi, sorry, the taskLIst Killed me.

--- src/main/java/tasklist/RepeatingTask.java
+++ src/main/java/tasklist/RepeatingTask.java
@@ -1,7 +1,7 @@
 package tasklist;
 
 /*FUNCIONALIDAD DE LAS TAREAS QUE SE REPITEN*/
-
+/*
 public abstract class RepeatingTask extends TaskListAbstract {
 	
 	//Constructor
@@ -13,4 +13,39 @@ public abstract class RepeatingTask extends TaskListAbstract {
 	public String toString() {
 		return "[REpetitiva]" + taskName + "-" + taskDescription + "(" + taskDescription + ")";
 	}
+}
+*/
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class RepeatingTask extends TaskListAbstract {
+
+    public RepeatingTask(String taskName, String taskDescription) {
+        super(taskName, taskDescription);
+    }
+
+    @Override
+    public void saveToDatabase(String dueDate) {
+        String sql = "INSERT INTO tareas (titulo, descripcion, tipo, due_date, status) VALUES (?, ?, ?, ?, ?)";
+        try (Connection con = getConnection(); 
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            
+            ps.setString(1, this.taskName);
+            ps.setString(2, this.taskDescription);
+            ps.setString(3, "Repetitiva");
+            ps.setString(4, dueDate);
+            ps.setString(5, "sin hacer");
+            
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getDetails() {
+        return "[Repetitiva] " + taskName;
+    }
 }
